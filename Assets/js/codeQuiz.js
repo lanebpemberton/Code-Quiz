2a07eaa647530dcd726fb9f9f5466790c50c7919
Lane Pemberton <lanebpemberton@gmail.com>
2021-03-11 19:55:56 -0500

    Added quiz container and begining code for quiz button function

--- Assets/js/codeQuiz.js
+++ Assets/js/codeQuiz.js
@@ -5,13 +5,23 @@ var highscoresButton = $("#highscoresButton");
 //function that calls everytime quiz button is selected
 function onQuizButton()
 {
-    console.log("quiz button");
+    //show quiz
+    var quizContainer = $("#quiz");
+    quizContainer.show();
+    //hide highscores
+    var highscoresContainer = $("#highscores");
+    highscoresContainer.hide();
 }
 
 //function that calls everytime highscores button is selected
 function onHighscoresButton()
 {
-    console.log("highscores button");
+    //show highscores
+    var highscoresContainer = $("#highscores");
+    highscoresContainer.show();
+    //hide quiz
+    var quizContainer = $("#quiz");
+    quizContainer.hide();
 }
 
 //setup on click events using jquery
